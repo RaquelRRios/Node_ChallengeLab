1fede3fe35b14475f85cf3e5425b6636228343e1
RaquelRRios <raquelribeirorios93@gmail.com>
2020-12-02 22:09:13 -0300

    Adicionando cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const Koa = require("koa");
 const bodyparser = require("koa-bodyparser");
 const router = require("./src/routes");
+const cors = require("@koa/cors");
 
 const server = new Koa();
 
+const options = { origin: "*" };
+
+server.use(cors(options));
 server.use(bodyparser());
 server.use(router.routes());
 
-server.listen(8081, () => console.log("Rodando!"));
+server.listen(8081, "0.0.0.0", () => console.log("Rodando!"));
